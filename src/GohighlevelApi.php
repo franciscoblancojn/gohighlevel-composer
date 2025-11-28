f4d8ce2bfb57a919c14ee90db1e5959bce27b1a9
franciscoblancojn <blancofrancisco34@gmail.com>
2025-11-28 11:33:20 -0500

    fix ssl

--- src/GohighlevelApi.php
+++ src/GohighlevelApi.php
@@ -36,6 +36,8 @@ class GohighlevelApi
             curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
             curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
 
             if ($body !== null) {
                 curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($body));
